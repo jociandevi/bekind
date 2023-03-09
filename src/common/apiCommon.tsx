4e0582cbfa9b30b4eb4b0338095b541baeb14d4b
Évi Kádár <evi.kadar@cloudera.com>
2023-03-09 09:55:39 +0100

    implementing actual backend call

---
+++ src/common/apiCommon.tsx
@@ -0,0 +1,69 @@
+import axios from "axios";
+
+interface Params {
+  baseUrl: string;
+  headers: any;
+  method: string;
+  mode?: string;
+}
+
+const postConfig: Params = {
+  baseUrl: "https://pwatester.azurewebsites.net",
+  headers: {
+    Authorization: "",
+  },
+  method: "post",
+};
+
+const getConfig: Params = {
+  baseUrl: "https://pwatester.azurewebsites.net",
+  headers: {
+    // Authorization: "",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+    "Access-Control-Allow-Headers":
+      "X-Requested-With, content-type, Authorization",
+    "Access-Control-Allow-Credentials": true,
+  },
+  method: "get",
+  mode: "no-cors",
+};
+
+export const postAPI = async (url: string, data: any): Promise<any> => {
+  return await axios({
+    ...postConfig,
+    url: `${postConfig.baseUrl}/${url}`,
+    data,
+  })
+    .then((response) => {
+      return {
+        status: response.status,
+        data: response.data,
+      };
+    })
+    .catch((error) => {
+      return {
+        status: error.status,
+        data: error.response,
+      };
+    });
+};
+
+export const getAPI = async (url: string): Promise<any> => {
+  return await axios({
+    ...getConfig,
+    url: `${getConfig.baseUrl}/${url}`,
+  })
+    .then((response) => {
+      return {
+        status: response.status,
+        data: response.data,
+      };
+    })
+    .catch((error) => {
+      return {
+        status: error.status,
+        data: error.response,
+      };
+    });
+};
--- src/components/pickmeup/parent.tsx
+++ src/components/pickmeup/parent.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { getAPI, postAPI } from "../../common/apiCommon";
 import {
   StyledButton,
   StyledContainer,
@@ -8,17 +10,59 @@ import { variables } from "../shared/variables";
 const Parent: React.FC = () => {
   // identify logged in user
   const user = {
+    id: "1",
     firstName: "Evi",
     lastName: "Kadar",
     canPickUp: [{ firstName: "Dani", lastName: "Hyross" }],
   };
 
+  const [weather, setWeather] = React.useState([
+    { temperatureC: 1, summary: "Questionable" },
+  ]);
+
+  //function below triggers the helper function
+  const getData = () =>
+    getAPI("weatherforecast").then((res) => {
+      if (res.status === 200) {
+        setWeather(res.data);
+        console.log(weather);
+      } else {
+        console.log(res);
+      }
+    });
+
+  //this runs the getData trigger function as useEffect
+  React.useEffect(() => {
+    getData();
+  }, []);
+
+  const sendHome = () => {
+    const now = new Date();
+    const pickupEvent = {
+      parent: user.id,
+      kid: user.canPickUp[0],
+      arrivedAt: now.toISOString(),
+    };
+    postAPI("pickupEvents", pickupEvent).then((res) => {
+      console.log(res);
+    });
+  };
+
   return (
     <StyledContainer>
       <StyledTitle level={3}>Hi {user.firstName}!</StyledTitle>
-      <StyledButton backgroundcolor={variables.blue1} type="primary">
+      <StyledButton
+        backgroundcolor={variables.blue1}
+        type="primary"
+        onClick={sendHome}
+      >
         PICK UP {user.canPickUp[0].firstName.toUpperCase()}
       </StyledButton>
+
+      <StyledTitle level={4}>
+        Today it is {weather[0].temperatureC} °C. What a {weather[0].summary}{" "}
+        weather!
+      </StyledTitle>
     </StyledContainer>
   );
 };
