20db1ecec8596c73d99e3032cd9e7905a69ae82f
evikadar <evi.kadar@gmail.com>
2023-09-11 15:00:34 +0200

    Take 2 - Putting beforeinstallprompt into component

--- src/components/shared/installButton.tsx
+++ src/components/shared/installButton.tsx
@@ -1,24 +1,62 @@
 import React, { useEffect, useState } from "react";
 import { Button } from "antd";
 import { DownloadOutlined } from "@ant-design/icons";
-import { useInstallPwaPrompt } from "../../common/useInstallPrompt";
+
+interface BeforeInstallPromptEvent extends Event {
+  readonly platforms: string[];
+  readonly userChoice: Promise<{
+    outcome: "accepted" | "dismissed";
+    platform: string;
+  }>;
+  prompt(): Promise<void>;
+}
 
 const InstallButton: React.FC = () => {
-  const [prompt, promptToInstall] = useInstallPwaPrompt();
+  const [deferredPrompt, setDeferredPrompt] =
+    useState<BeforeInstallPromptEvent | null>(null);
   const [isVisible, setVisibleState] = useState(false);
 
   useEffect(() => {
-    if (prompt) {
+    const handleBeforeInstallPrompt = (event: BeforeInstallPromptEvent) => {
+      // Prevent the default browser install prompt
+      event.preventDefault();
+
+      // Stash the event so it can be triggered later.
+      setDeferredPrompt(event);
       setVisibleState(true);
-    }
-  }, [prompt]);
+    };
+
+    (window as any).addEventListener(
+      "beforeinstallprompt",
+      handleBeforeInstallPrompt
+    );
+
+    return () => {
+      (window as any).removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt
+      );
+    };
+  }, [deferredPrompt]);
 
   if (!isVisible) {
     return null;
   }
 
+  const onClick = () => {
+    deferredPrompt?.prompt();
+    deferredPrompt?.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the PWA installation");
+      } else {
+        console.log("User declined the PWA installation");
+      }
+    });
+    setDeferredPrompt(null);
+  };
+
   return (
-    <Button icon={<DownloadOutlined />} onClick={promptToInstall}>
+    <Button icon={<DownloadOutlined />} onClick={onClick}>
       Install
     </Button>
   );
