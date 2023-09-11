5bc148a809c2789e2d5809e252f306c573515b11
evikadar <evi.kadar@gmail.com>
2023-09-11 09:05:40 +0200

    Adding 512x512 icon

---
+++ src/common/notificationService.tsx
@@ -0,0 +1,8 @@
+export const checkIfUserIsSubscribed = () => {
+  // make an API call to our backend to check if user is subscribed
+  return null;
+};
+
+export const subscribeUser = () => {
+  // make an API call to our backend to subscribe user
+};
