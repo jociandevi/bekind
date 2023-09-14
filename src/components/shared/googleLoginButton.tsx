2810304a2d3c048fe43a9dec1f14543af43cf788
evikadar <evi.kadar@gmail.com>
2023-09-14 08:07:24 +0200

    Moving the google client ID ito an env file

--- src/components/shared/googleLoginButton.tsx
+++ src/components/shared/googleLoginButton.tsx
@@ -20,8 +20,7 @@ const GoogleLoginButton: React.FC = () => {
     };
 
     google.accounts.id.initialize({
-      client_id:
-        "1089149343789-4eck6r05fbi0nesua0lkhn4udf7d5303.apps.googleusercontent.com",
+      client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID || "",
       callback: handleCallbackResponse,
     });
 
