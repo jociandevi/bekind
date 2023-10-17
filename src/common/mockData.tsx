6e8434b74912a1f0b4e08ca8e1dade190b7679f9
evikadar <evi.kadar@gmail.com>
2023-10-17 14:36:14 +0200

    1 article

--- src/common/mockData.tsx
+++ src/common/mockData.tsx
@@ -592,6 +592,16 @@ export const raoks: KindnessAction[] = [
     credit: "Unsplash - Nik",
     duration: 60,
   },
+  {
+    id: 57,
+    title: "Exercise",
+    description: "And feel better instantly",
+    category: CategoryNames.YOU,
+    imageUrl:
+      "https://images.unsplash.com/photo-1583873743670-6d60e445a7e2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
+    credit: "Unsplash - Anton Mislawsky",
+    duration: 60,
+  },
 ];
 
 export const user = {
