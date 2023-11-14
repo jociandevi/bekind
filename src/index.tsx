bb45b494a055dd8c8708b3835e5e54691a0c9ea1
evikadar <evi.kadar@gmail.com>
2023-11-14 14:16:56 +0100

    Changing the import's order

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
+import "react-app-polyfill/stable";
+import "core-js/stable";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
-import "react-app-polyfill/stable";
-import "core-js/stable";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
